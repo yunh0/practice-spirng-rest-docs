7b5f262330cd11020167c4fb0f14b05ab57953d7
YunhoNam <nam100400@naver.com>
2024-07-04 23:03:01 +0900

    Project init

---
+++ src/main/java/com/yunho/practicespringrestdocs/PracticeSpringRestDocsApplication.java
@@ -0,0 +1,13 @@
+package com.yunho.practicespringrestdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringRestDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpringRestDocsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yunho/practicespringrestdocs/PracticeSpringRestDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunho.practicespringrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringRestDocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
